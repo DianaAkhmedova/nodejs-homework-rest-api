f47e070503578b8307edb6badb054c748451d9fc
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-04 21:45:09 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,8 +2,8 @@ const express = require("express");
 
 const router = express.Router();
 
-const { asyncHandler } = require("../../controllers");
-const { addSchema } = require("../../schemas");
+const asyncHandler = require("../../controllers");
+const addSchema = require("../../schemas");
 
 const {
   listContacts,
