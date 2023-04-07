78fdff063ba6544624a3e5f312f9c552ca29e678
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-08 00:15:14 +0300

    Update getAllContacts.js

--- controllers/getAllContacts.js
+++ controllers/getAllContacts.js
@@ -1,8 +1,8 @@
-const { listContacts } = require("../models");
+const Contact = require("../models");
 const ctrlWrapper = require("../utils");
 
 const getAllContacts = async (req, res) => {
-  res.json(await listContacts());
+  res.json(await Contact.find());
 };
 
 module.exports = { getAllContacts: ctrlWrapper(getAllContacts) };
