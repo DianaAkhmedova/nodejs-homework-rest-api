30d2d05924604ceff0be89b8734e3cb1e9c204f3
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-08 00:15:47 +0300

    Update contacts.js

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -14,4 +14,6 @@ router.delete("/:contactId", ctrl.removeContact);
 
 router.put("/:contactId", ctrl.updateContact);
 
+router.patch("/:contactId/favorite", ctrl.updateFavorite);
+
 module.exports = router;
