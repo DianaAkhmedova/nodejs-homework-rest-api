17c633817c4ee3d9fc5fb0a767c79ce2b812e83f
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-08 00:16:58 +0300

    Update index.js

--- schemas/index.js
+++ schemas/index.js
@@ -1,3 +1,7 @@
-const addSchema = require("./addSchema");
+const {
+  contactSchema,
+  addSchema,
+  updateFavoriteSchema,
+} = require("./contactSchema");
 
-module.exports = addSchema;
+module.exports = { contactSchema, addSchema, updateFavoriteSchema };
