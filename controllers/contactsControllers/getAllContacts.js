b84c42d95c37a030aee29bcbe4577e688961533b
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-19 17:27:12 +0300

    Update getAllContacts.js

--- controllers/contactsControllers/getAllContacts.js
+++ controllers/contactsControllers/getAllContacts.js
@@ -2,7 +2,25 @@ const { Contact } = require("../../models");
 const { ctrlWrapper } = require("../../utils");
 
 const getAllContacts = async (req, res) => {
-  res.json(await Contact.find());
+  const { _id: owner } = req.user;
+  const { page = 1, limit = 20, favorite } = req.query;
+  const skip = (page - 1) * limit;
+
+  if (Boolean(favorite)) {
+    const result = await Contact.find({ owner, favorite }, "", {
+      skip,
+      limit,
+    }).populate("owner", "email");
+
+    res.json(result);
+  }
+
+  res.json(
+    await Contact.find({ owner }, "", { skip, limit }).populate(
+      "owner",
+      "email"
+    )
+  );
 };
 
 module.exports = { getAllContacts: ctrlWrapper(getAllContacts) };
