188d877398602009279ea1f083902a5565ab4230
DianaAkhmedova <106441711+DianaAkhmedova@users.noreply.github.com>
2023-04-19 17:27:24 +0300

    Update auth-routes.js

--- routes/api/auth-routes.js
+++ routes/api/auth-routes.js
@@ -3,16 +3,30 @@ const express = require("express");
 const router = express.Router();
 
 const { validateBody } = require("../../utils");
+
+const { authenticate } = require("../../middlewares");
+
 const {
-  userSchema,
   registerSchema,
   loginSchema,
   updateSubscriptionSchema,
 } = require("../../schemas");
+
 const ctrl = require("../../controllers");
 
 router.post("/register", validateBody(registerSchema), ctrl.register);
 
 router.post("/login", validateBody(loginSchema), ctrl.login);
+
+router.get("/current", authenticate, ctrl.getCurrent);
+
+router.post("/logout", authenticate, ctrl.logout);
+
+router.patch(
+  "/",
+  authenticate,
+  validateBody(updateSubscriptionSchema),
+  ctrl.updateSubscription
+);
 
 module.exports = router;
